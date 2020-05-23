615c0b342c35aa4310d74fd0de0bb93e84d4161d
idan asulin <djidanasulin@gmail.com>
2020-05-23 20:36:43 +0300

    fixes

--- src/export.js
+++ src/export.js
@@ -1,16 +1,8 @@
 export * from './components/CustomHeader';
 export * from './CustomDrawerContent';
-
 export * from './screens/TestScreen';
 export * from './screens/InstructionScreen';
-export * from './screens/TestProcessScreen';
-
 export * from './screens/RehabilitionScreen';
 export * from './screens/VideoDetailesScreen';
-
 export * from './drawer/NotificationsScreen';
-
-export * from './components/CustomHeader';
-
 export * from './auth/LoginScreen';
-
